7e7cedbfbe889ee262bdd77f96c6d192299dc8a8
janpiter16 <janpurba90@gmail.com>
2023-11-02 21:50:33 +0700

    update dev b

--- resep.php
+++ resep.php
@@ -1,4 +1,8 @@
-print("Resep Krabby Patty")
-print("------------------")
-print("1. bawang")
-print("2. kentang") 
+<?php
+echo ("Resep Krabby Patty");
+echo ("------------------");
+echo ("1. bawang");
+echo ("2. kentang");
+echo ("3. roti");
+echo ("4. keju");
+echo ("5. tomat");
